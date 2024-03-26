cdcfd014f1a308fa01dae3a8b837ef367148dff9
Teo Calvo <teocalvo2@gmail.com>
2024-03-26 11:05:43 -0300

    Jovem Nerd e TabNews

---
+++ JovemNerd/episodios.py
@@ -0,0 +1,82 @@
+# %%
+
+import requests
+import datetime
+import json
+import pandas as pd
+import time
+
+# ?per_page=1000&page=1
+
+# %%
+
+class Collector:
+
+    def __init__(self, url, instance_name):
+        self.url = url
+        self.instance_name = instance_name
+
+
+    def get_content(self, **kwargs):
+        resp = requests.get(self.url, params=kwargs)
+        return resp
+
+
+    def save_parquet(self, data):
+        now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S.%f")
+        df = pd.DataFrame(data)
+        df.to_parquet(f"data/{self.instance_name}/parquet/{now}.parquet", index=False)
+
+
+    def save_json(self, data):
+        now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S.%f")
+        with open(f"data/{self.instance_name}/json/{now}.json", "w") as open_file:
+            json.dump(data, open_file)
+
+
+    def save_data(self, data, format='json'):
+        if format == 'json':
+            self.save_json(data)
+
+        elif format == "parquet":
+            self.save_parquet(data)
+
+
+    def get_and_save(self, save_format='json', **kwargs):
+        resp = self.get_content(**kwargs)
+        if resp.status_code == 200:
+            data = resp.json()
+            self.save_data(data, save_format)
+        else:
+            data = None
+            print(f"Request sem sucesso: {resp.status_code}", resp.json())
+        return data
+
+
+    def auto_exec(self,save_format='json', date_stop='2000-01-01'):
+        page = 1
+        while True:
+            print(page)
+            data = self.get_and_save(save_format=save_format,
+                                     page=page,
+                                     per_page=1000)
+            if data == None:
+                print("Erro ao coletar dados... aguardando.")
+                time.sleep(60*5)
+            
+            else:
+                date_last = pd.to_datetime(data[-1]["published_at"]).date()
+                if date_last<pd.to_datetime(date_stop).date():
+                    break
+                elif len(data)<1000:
+                    break
+                page+=1
+                time.sleep(5)
+
+# %%
+
+url = "https://api.jovemnerd.com.br/wp-json/jovemnerd/v1/nerdcasts/"
+collect = Collector(url, "episodios")
+collect.auto_exec()
+
+# %%
---
+++ TabNews/basic_content.py
@@ -0,0 +1,51 @@
+# %%
+import datetime
+import json
+import time
+
+import pandas as pd
+import requests
+
+# %%
+
+def get_response(**kwargs):
+    url = "https://www.tabnews.com.br/api/v1/contents/"
+    resp = requests.get(url, params=kwargs)
+    return resp
+
+
+def save_data(data, option='json'):
+    now = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S.%f")
+    
+    if option == 'json':
+        with open(f"data/contents/json/{now}.json", 'w') as open_file:
+            json.dump(data, open_file, indent=4)
+
+    elif option == 'parquet':
+        df = pd.DataFrame(data)
+        df.to_parquet(f"data/contents/parquet/{now}.parquet", index=False)
+
+
+# %%
+page = 1
+date_stop = pd.to_datetime('2024-03-01').date()
+while True:
+    print(page)
+    
+    resp = get_response(page=page, per_page=100, strategy="new")
+    if resp.status_code == 200:
+        data = resp.json()
+        save_data(data)
+
+        date = pd.to_datetime(data[-1]["updated_at"]).date()
+        if len(data) < 100 or date < date_stop:
+            break
+        
+        page += 1
+        time.sleep(5)
+    
+    else:
+        print(resp.status_code)
+        print(resp.json())
+        time.sleep(60 * 15)
+# %%
